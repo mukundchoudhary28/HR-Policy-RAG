73da67c9bcfc59f08c35851d10a1b5c06caad523
mukundchoudhary28 <mukund.ch28@gmail.com>
2026-04-28 09:10:55 +0530

    Modified/Removed irrelevant files

--- src/api/models.py
+++ src/api/models.py
@@ -2,19 +2,19 @@
 from pydantic import BaseModel, model_validator
 from typing import Literal, List, Optional, Dict
 
-class Filter(BaseModel):
-    doc_type: Optional[Literal['leave', 'holiday', 'payroll', 'benefits', 'conduct', 'work', 'general']] = None
-    year: Optional[int] = None
-    region: Optional[List[Literal['US', 'IN', 'UK', 'EU', 'APAC', 'Global']]] = None
-    is_latest: Optional[str] = None
+# class Filter(BaseModel):
+#     doc_type: Optional[Literal['leave', 'holiday', 'payroll', 'benefits', 'conduct', 'work', 'general']] = None
+#     year: Optional[int] = None
+#     region: Optional[List[Literal['US', 'IN', 'UK', 'EU', 'APAC', 'Global']]] = None
+#     is_latest: Optional[str] = None
 
-    @model_validator(mode="after")
-    def set_is_latest(self):
-        if self.year is not None:
-            self.is_latest = None
-        else:
-            self.is_latest = "Yes"
-        return self
+#     @model_validator(mode="after")
+#     def set_is_latest(self):
+#         if self.year is not None:
+#             self.is_latest = None
+#         else:
+#             self.is_latest = "Yes"
+#         return self
 
 class ResponseSchema(BaseModel):
     answer: str
